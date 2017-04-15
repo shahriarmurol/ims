4a96bf20bc73898a0c62c59e442767ed61b1dcf4
shahriarmurol <shahriarmurolcse@gmail.com>
2017-04-16 02:36:45 +0600

    home page my work markup finish

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,31 @@
 (function($){
-
-$('.marquee').marquee({
-    //speed in milliseconds of the marquee
-    duration: 5000,
-    //gap in pixels between the tickers
-    gap: 50,
-    //time in milliseconds before the marquee will start animating
-    delayBeforeStart: 0,
-    //'left' or 'right'
-    direction: 'left',
-    //true or false - should the marquee be duplicated to show an effect of continues flow
-    duplicated: true
-});
+    //js marquee
+    $('.marquee').marquee({
+        //speed in milliseconds of the marquee
+        duration: 5000,
+        //gap in pixels between the tickers
+        gap: 50,
+        //time in milliseconds before the marquee will start animating
+        delayBeforeStart: 0,
+        //'left' or 'right'
+        direction: 'left',
+        //true or false - should the marquee be duplicated to show an effect of continues flow
+        duplicated: true
+    });
+    //news tickers 
+    $('.ui-newsticker').newsticker();
+    // owl carasul
+    $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        autoplayHoverPause:true,
+        margin:10,
+        loop:true,
+        items:1,
+        mouseDrag:true,
+        touchDrag:true,
+        pullDrag:true,
+        freeDrag:true,
+        smartSpeed:true,
+    });
 
 })(jQuery);
